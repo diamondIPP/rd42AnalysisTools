c6efd95f0135025e47dd930019f1d50210a34087
diegoasanz <diegoasanz@gmail.com>
2018-11-19 11:45:04 +0100

    added options for using alignment and making tcutg for each cell

--- TransparentGrid.py
+++ TransparentGrid.py
@@ -18,12 +18,15 @@ class TransparentGrid:
         self.dir = os.path.abspath(os.path.expanduser(os.path.expandvars(dir)))
         self.trans_file = None
         self.trans_tree = None
+        self.align_file = None
+        self.align_obj = None
+        self.align_info = {'xoff': float(0), 'phi': float(0)}
         self.num_cols = 0
         self.ch_ini = 0
         self.ch_end = 0
         self.pkl = None
         self.loaded_pickle = False
-        self.row_info = {'0': float(0), 'm': float(0), 'num': 27, 'pitch': 50.0}
+        self.row_info = {'0': float(61.84669791829917), 'm': float(0.02551248435536136), 'num': 27, 'pitch': 0.999999901}
         self.vertical_lines = []
         self.vertical_lines_tline = []
         self.horizontal_lines = []
@@ -33,6 +36,9 @@ class TransparentGrid:
         self.canvas = {}
         self.profile = {}
         self.names = []
+        self.tcutgs = {}
+        self.goodAreas = []
+        self.badAreas = []
 
     def CheckFoldersAndFiles(self):
         if not os.path.isdir(self.dir):
@@ -60,12 +66,16 @@ class TransparentGrid:
                 self.pkl = pickle.load(pkl)
                 self.loaded_pickle = True
 
-    def SetLines(self):
+    def SetLines(self, try_align=False):
         self.TryLoadPickle()
         if self.loaded_pickle:
             self.row_info = self.pkl['row_info']
             self.vertical_lines = self.pkl['vertical_lines']
             self.horizontal_lines = self.pkl['horizontal_lines']
+            self.align_info = self.pkl['align_info']
+        elif try_align:
+            self.FindHorizontalParametersThroughAlignment()
+            # self.CreateLines()
         else:
             self.AskUserLowerYLine()
             self.CreateLines()
@@ -74,7 +84,7 @@ class TransparentGrid:
         self.row_info['0'] = self.GetFromUser('Enter the y axis intercept in silicon space for the lower detector limit (scalar between 0 and 255): ', typ='float', limits=[0, 255])
         self.row_info['m'] = self.GetFromUser('Enter the slope for the lower detector line (scalar between -1 and 1): ', typ='float', limits=[-1, 1])
         self.row_info['num'] = self.GetFromUser('Enter the number of rows: ', typ='int', limits=[1, 1000])
-        self.row_info['pitch'] = self.GetFromUser('Enter the effective pitch in sil space in Y axis: ', typ='float', limits=[0, 10000])
+        self.row_info['pitch'] = self.GetFromUser('Enter the effective pitch in sil space in Y axis: ', typ='float', limits=[0, 255])
 
     def GetFromUser(self, message, typ, limits=[]):
         cont = False
@@ -98,6 +108,16 @@ class TransparentGrid:
                     else:
                         cont = True
         return tempv
+
+    def FindHorizontalParametersThroughAlignment(self):
+        self.LoadAlignmentParameters()
+
+    def LoadAlignmentParameters(self):
+        if os.path.isfile('{d}/{r}/alignment.{r}.root'.format(d=self.dir, r=self.run)):
+            self.align_file = ro.TFile('{d}/{r}/alignment.{r}.root'.format(d=self.dir, r=self.run), 'r')
+            self.align_obj = self.align_file.Get('alignment')
+            self.align_info['xoff'] = self.align_obj.GetXOffset(4)
+            self.align_info['phi'] = self.align_obj.GetPhiXOffset(4)
 
     def CreateLines(self):
         linev = self.GetVerticalLine(x=self.ch_ini - 0.5)
@@ -126,7 +146,7 @@ class TransparentGrid:
             self.vertical_lines_tline[-1].SetLineColor(ro.kRed)
 
     def DrawProfile2D(self, name, varz):
-        self.profile[name] = ro.TProfile2D('h_'+name, 'h_'+name, int(256 * self.bins_per_ch_x), - 0.5, 255.5, int(256 * self.bins_per_ch_y), -0.5, 255.5)
+        self.profile[name] = ro.TProfile2D('h_'+name, 'h_'+name, int(128 * self.bins_per_ch_x), - 0.5, 127.5, int(256 * self.bins_per_ch_y), -0.5, 255.5)
         self.canvas[name] = ro.TCanvas('c_'+name, 'c_'+name, 1)
         self.canvas[name].cd()
         self.trans_tree.Draw('{z}:fidY:diaChXPred>>h_{n}'.format(z=varz, n=name), 'transparentEvent', 'colz prof')
@@ -138,18 +158,50 @@ class TransparentGrid:
         for linev in self.vertical_lines_tline:
             linev.Draw('same')
 
+    def CreateTCutGs(self):
+        def GetNumpyArraysX(coli):
+            x0 = self.ch_ini - 0.5 + coli
+            x1 = self.ch_ini + 0.5 + coli
+            return np.array((x0, x0, x1, x1, x0), 'f8')
+        def GetNumpyArraysY(coli, rowi):
+            y0 = self.row_info['0'] + self.row_info['m'] * (self.ch_ini - 0.5 + coli) + rowi * self.row_info['pitch']
+            y1 = self.row_info['0'] + self.row_info['m'] * (self.ch_ini - 0.5 + coli) + (rowi + 1) * self.row_info['pitch']
+            y2 = self.row_info['0'] + self.row_info['m'] * (self.ch_ini + 0.5 + coli) + (rowi + 1) * self.row_info['pitch']
+            y3 = self.row_info['0'] + self.row_info['m'] * (self.ch_ini + 0.5 + coli) + rowi * self.row_info['pitch']
+            return np.array((y0, y1, y2, y3, y0), 'f8')
+        for col in xrange(self.num_cols):
+            self.tcutgs[col] = {}
+            for row in xrange(self.row_info['num']):
+                tempx = GetNumpyArraysX(col)
+                tempy = GetNumpyArraysY(col, row)
+                self.tcutgs[col][row] = ro.TCutG('cutg_{c}_{r}'.format(c=col, r=row), 5, tempx, tempy)
+                self.tcutgs[col][row].SetNameTitle('cutg_{c}_{r}'.format(c=col, r=row), 'cutg_{c}_{r}'.format(c=col, r=row))
+                self.tcutgs[col][row].SetVarX('diaChXPred')
+                self.tcutgs[col][row].SetVarY('fidY')
+                self.tcutgs[col][row].SetLineColor(ro.kRed)
+
+    def AddGoodAreas(self, col, row):
+        self.goodAreas.append(self.tcutgs[col][row])
+
+    def DrawGoodAreas(self, name):
+        self.canvas[name].cd()
+        for area in self.goodAreas:
+            area.Draw('same')
+
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-d', '--dir', dest='dir', type='string', help='Path to the subdirectory that contains the output of different runs')
     parser.add_option('-r', '--run', dest='run', type='int', help='run number to be analysed (e.g. 25209)')
+    parser.add_option('-a', '--al', dest='al', action='store_true', default=False, help='enable find grid through data and alignment')
 
     (options, args) = parser.parse_args()
     run = int(options.run)
     dir = str(options.dir)
+    use_align = bool(options.al)
 
     tg = TransparentGrid(dir=dir, run=run)
     tg.CheckFoldersAndFiles()
     tg.OpenFileAndGetTree()
     tg.FindDiamondChannelLimits()
-    tg.SetLines()
+    tg.SetLines(try_align=use_align)
