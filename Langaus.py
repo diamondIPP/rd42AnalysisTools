953d0b586032d05ccd7973606286f877ca60ec1b
diegoasanz <diegoasanz@gmail.com>
2018-11-26 11:58:00 +0100

    fixed Langaus fit

--- Langaus.py
+++ Langaus.py
@@ -17,7 +17,7 @@ class LanGaus:
 		self.histo = histo
 		self.chi2 = 0
 		self.ndf = 0
-		self.fit_range = [histo.GetBinLowEdge(histo.FindFirstBinAbove()), histo.GetBinLowEdge(histo.FindLastBinAbove()) + histo.GetBinWidth(histo.FindLastBinAbove())]
+		self.fit_range = [max(self.histo.GetMean()*0.3, 0), min(self.histo.GetXaxis().GetXmax(), self.histo.GetMean()*3)]
 		self.fit = None
 		self.EstimateParameters()
 		self.EstimateParametersLimits()
@@ -27,33 +27,31 @@ class LanGaus:
 		xlow, xhigh = [x[0] + self.sigma_conv * i * params[3] for i in [-1, 1]]
 		step = (xhigh - xlow) / self.conv_steps
 		sums = 0
-		i = 1
-		while i <= int(np.ceil(self.conv_steps/2.0)):
+		for i in xrange(1, int(np.ceil(self.conv_steps/2.0 + 1))):
 			xx = xlow + (i - 0.5) * step
 			fland = ro.TMath.Landau(xx, mpc, params[0]) / params[0]
-			sums += fland + ro.TMath.Gaus(x[0], xx, params[3])
+			sums += fland * ro.TMath.Gaus(x[0], xx, params[3])
 			xx = xhigh - (i - 0.5) * step
 			fland = ro.TMath.Landau(xx, mpc, params[0]) / params[0]
 			sums += fland * ro.TMath.Gaus(x[0], xx, params[3])
-			i += 1
 		return params[2] * step * sums / (np.sqrt(2 * np.pi, dtype='f8') * params[3])
 
 
 	def EstimateParameters(self):
-		self.params[0] = self.histo.GetRMS()/5.74
-		self.params[1] = ((self.histo.GetMean() + self.histo.GetBinCenter(self.histo.GetMaximumBin())) / 2.0) + self.mpshift * self.params[0]
-		self.params[2] = self.histo.Integral() * 10
-		self.params[3] = self.histo.GetRMS() * 3.55
+		self.params[0] = self.histo.GetRMS()/5.7
+		self.params[1] = self.histo.GetBinCenter(self.histo.GetMaximumBin())
+		self.params[2] = self.histo.Integral() * 500  # 10
+		self.params[3] = self.histo.GetRMS() / 3.4
 
 	def EstimateParametersLimits(self):
-		self.paramsLimitsLow[0] = self.histo.GetRMS()/10.0
-		self.paramsLimitsLow[1] = max(0, self.histo.GetBinCenter(self.histo.GetMaximumBin()) + self.mpshift * 2*self.params[0])
-		self.paramsLimitsLow[2] = self.histo.Integral() * 5.0
-		self.paramsLimitsLow[3] = self.histo.GetRMS() * 1.5
+		self.paramsLimitsLow[0] = self.histo.GetRMS()/15.0
+		self.paramsLimitsLow[1] = max(0, self.histo.GetBinCenter(self.histo.GetMaximumBin()) - 3*self.params[0])
+		self.paramsLimitsLow[2] = self.histo.Integral() / 2.0
+		self.paramsLimitsLow[3] = self.histo.GetRMS() / 5.0
 		self.paramsLimitsHigh[0] = self.histo.GetRMS()/4.0
-		self.paramsLimitsHigh[1] = min(self.histo.GetXaxis().GetXmax(), self.histo.GetBinCenter(self.histo.GetMaximumBin()) - self.mpshift * 2*self.params[0])
-		self.paramsLimitsHigh[2] = self.histo.Integral() * 15
-		self.paramsLimitsHigh[3] = self.histo.GetRMS()*5.0
+		self.paramsLimitsHigh[1] = min(self.histo.GetXaxis().GetXmax(), self.histo.GetBinCenter(self.histo.GetMaximumBin()) + 3*self.params[0])
+		self.paramsLimitsHigh[2] = self.histo.Integral() * 5000
+		self.paramsLimitsHigh[3] = self.histo.GetRMS()
 
 	def LanGausFit(self, nconv=1000):
 		fit_name = 'fit_{n}'.format(n=self.histo.GetName())
@@ -70,10 +68,22 @@ class LanGaus:
 
 		for i in xrange(len(self.params)):
 			self.fit.SetParLimits(i, self.paramsLimitsLow[i], self.paramsLimitsHigh[i])
-		ipdb.set_trace()
-		self.histo.Fit(fit_name, 'QRB0', '', self.fit_range[0], self.fit_range[1])
+		self.histo.Fit(fit_name, 'QRB0L', '', self.fit_range[0], self.fit_range[1])
 		self.fit.GetParameters(self.params)
 		for i in xrange(len(self.params)):
 			self.paramsFitErrors[i] = self.fit.GetParError(i)
 		self.chi2 = self.fit.GetChisquare()
 		self.ndf = self.fit.GetNDF()
+
+if __name__ == '__main__':
+	mu = 1000
+	sigma = 20
+	gaus_sigma = 5
+	rand = ro.TRandom3()
+	rand.SetSeed(0)
+	datad = np.array([rand.Landau(mu, sigma) + rand.Gaus(0, gaus_sigma) for i in xrange(1000)])
+	datah = ro.TH1F('datah', 'datah', 200, 0, 4000)
+	for point in datad:
+		datah.Fill(point)
+	lg = LanGaus(histo=datah)
+	lg.LanGausFit(nconv=100)
