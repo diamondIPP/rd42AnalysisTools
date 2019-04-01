entered condition to avoid errors in initial cm calculation when it is 0. Tested different definitions for IsMasked channels, and sticked with noisy, screened (masked) and non connected. Other fixes

PedestalCalculations.py
@@ -127,12 +127,13 @@ class PedestalCalculations(mp.Process):
         # self.rootFile, self.rootTree = Open_RootFile_Load_Tree('{d}/{s}/{r}/{f}.root'.format(d=self.out_dir, s=self.sub_dir, r=self.run, f=self.file_name), treename=self.tree_name, mode='READ')
         # self.rootTree.SetEstimate(256 * self.rootTree.GetEntries())
         time0 = time.time()
-        print 'Getting all events for DUT...', ; sys.stdout.flush()
+        if self.showprogress:
+            print 'Getting all events for DUT...', ; sys.stdout.flush()
 
         Draw_Branches_For_Get_Val(self.rootTree, [self.raw_dut_branch], 0, self.events, option='goff')
         temp = np.zeros((self.chs, self.events), dtype=self.dut_np_data_type)
         Get_Branches_Value_To_Numpy(self.rootTree, [self.raw_dut_branch], [temp], self.events, self.chs)
-        print 'Done in', time.time() - time0, 'seconds'
+        if self.showprogress: print 'Done in', time.time() - time0, 'seconds'
         return mp.Array(np.ctypeslib.as_ctypes(temp)._type_, np.ctypeslib.as_ctypes(temp))
 
     def run(self):
@@ -150,7 +151,7 @@ class PedestalCalculations(mp.Process):
                     # temp = PedestalDeviceCalculations('{d}/{s}/{r}/{f}.settings'.format(d=self.out_dir, s=self.sub_dir, r=self.run, f=self.file_name), device, True, self.dut_ADC_all_mp, self.dut_ADC_mean_all_mp, self.dut_ADC_sigma_all_mp, self.dut_ADC_is_ped_all_mp, self.dut_ADC_is_hit_all_mp, self.dut_ADC_is_seed_all_mp, self.dut_ADC_chs_cm_all_mp, self.dut_ADC_cm_all_mp, self.dut_ADC_mean_cmc_all_mp, self.dut_ADC_sigma_cmc_all_mp, self.dut_ADC_is_ped_cmc_all_mp, self.dut_ADC_is_hit_cmc_all_mp, self.dut_ADC_is_seed_cmc_all_mp, -1)
 
                 else:
-                    print 'Calculating pedestals for', device
+                    # print 'Calculating pedestals for', device
                     temp = PedestalDeviceCalculations(self.out_dir, self.run_no, self.hit_fact, self.seed_fact, self.dut_np_data_type, self.chs, self.events, self.diaNcChs, self.diaNoisyChs, self.diaMaskedChs, self.slide_leng, self.cm_cut, device, False, self.dut_ADC_all_mp, self.dut_ADC_mean_all_mp, self.dut_ADC_sigma_all_mp, self.dut_ADC_is_ped_all_mp, self.dut_ADC_is_hit_all_mp, self.dut_ADC_is_seed_all_mp, self.dut_ADC_chs_cm_all_mp, self.dut_ADC_cm_all_mp, self.dut_ADC_mean_cmc_all_mp, self.dut_ADC_sigma_cmc_all_mp, self.dut_ADC_is_ped_cmc_all_mp, self.dut_ADC_is_hit_cmc_all_mp, self.dut_ADC_is_seed_cmc_all_mp, -1)
                     # temp = PedestalDeviceCalculations(self.out_dir, self.run_no, self.hit_fact, self.seed_fact, self.dut_np_data_type, self.chs, self.events, self.diaNcChs, self.diaNoisyChs, self.diaMaskedChs, self.slide_leng, self.cm_cut, device, False, self.dut_ADC_all_mp, self.dut_ADC_mean_all_mp, self.dut_ADC_sigma_all_mp, self.dut_ADC_is_ped_all_mp, self.dut_ADC_is_hit_all_mp, self.dut_ADC_is_seed_all_mp, self.dut_ADC_chs_cm_all_mp, self.dut_ADC_cm_all_mp, self.dut_ADC_mean_cmc_all_mp, self.dut_ADC_sigma_cmc_all_mp, self.dut_ADC_is_ped_cmc_all_mp, self.dut_ADC_is_hit_cmc_all_mp, self.dut_ADC_is_seed_cmc_all_mp, self.dut_ADC_signal_all_mp, self.dut_ADC_signal_cmc_all_mp, -1)
                     # temp = PedestalDeviceCalculations('{d}/{s}/{r}/{f}.settings'.format(d=self.out_dir, s=self.sub_dir, r=self.run, f=self.file_name), device, False, self.dut_ADC_all_mp, self.dut_ADC_mean_all_mp, self.dut_ADC_sigma_all_mp, self.dut_ADC_is_ped_all_mp, self.dut_ADC_is_hit_all_mp, self.dut_ADC_is_seed_all_mp, self.dut_ADC_chs_cm_all_mp, self.dut_ADC_cm_all_mp, self.dut_ADC_mean_cmc_all_mp, self.dut_ADC_sigma_cmc_all_mp, self.dut_ADC_is_ped_cmc_all_mp, self.dut_ADC_is_hit_cmc_all_mp, self.dut_ADC_is_seed_cmc_all_mp, -1)
@@ -167,7 +168,8 @@ class PedestalCalculations(mp.Process):
 
 
     def SetBranches(self):
-        print 'Setting branches in tree...', ; sys.stdout.flush()
+        if self.showprogress:
+            print 'Setting branches in tree...', ; sys.stdout.flush()
         # self.outTree.Branch('diaChPedNoCmc', self.dut_ADC_is_ped, 'diaChPedNoCmc[{chs}]/O'.format(chs=self.chs))
         # self.outTree.Branch('diaChHitsNoCmc', self.dut_ADC_is_hit, 'diaChHitsNoCmc[{chs}]/O'.format(chs=self.chs))
         # self.outTree.Branch('diaChSeedNoCmc', self.dut_ADC_is_seed, 'diaChSeedNoCmc[{chs}]/O'.format(chs=self.chs))
@@ -195,11 +197,11 @@ class PedestalCalculations(mp.Process):
         # self.b15 = self.outTree.Branch('diaSeedChsCmc', self.dut_ADC_is_seed_cmc, 'diaSeedChsCmc[{chs}]/O'.format(chs=self.chs))
         # self.b16 = self.outTree.Branch('diaPedestalMeanCmc', self.dut_ADC_mean_cmc, 'diaPedestalMeanCmc[{f}]/F'.format(f=self.chs))
         # self.b17 = self.outTree.Branch('diaPedestalSigmaCmc', self.dut_ADC_sigma_cmc, 'diaPedestalSigmaCmc[{f}]/F'.format(f=self.chs))
-        print 'Done'
+        if self.showprogress: print 'Done'
 
     def FillTree(self):
-        print 'Filling tree...'
         if self.showprogress:
+            print 'Filling tree...'
             nevents = int(self.ev_end - self.ev_ini + 1)
             bar = CreateProgressBarUtils(nevents)
             bar.start()

PedestalDeviceCalculations.py
@@ -94,7 +94,10 @@ class PedestalDeviceCalculations(mp.Process):
         # channels that are masked for common mode calculation because they are screened or noisy
 
         # self.is_not_masked = np.array([ch not in set(self.settings.noisy) | set(self.settings.screened) for ch in xrange(self.chs)], '?')
-        self.is_not_masked = np.bitwise_not(np.bitwise_or(self.noisy_array, self.masked_array))
+        self.is_not_masked = np.bitwise_not(np.bitwise_or(np.bitwise_or(self.noisy_array, self.masked_array), self.nc_array))
+        # self.is_not_masked = np.bitwise_not(np.bitwise_or(self.noisy_array, self.masked_array))
+        # self.is_not_masked = np.bitwise_not(np.bitwise_or(self.noisy_array, self.nc_array))
+        # self.is_not_masked = np.bitwise_not(self.noisy_array)
 
         # global variables passed as reference for shared memory ctype vectors
         global in_adc_array, out_array_m, out_array_s, out_array_is_p, out_array_is_h, out_array_is_s
@@ -359,7 +362,7 @@ class PedestalDeviceCalculations(mp.Process):
             adc_cond = [np.extract(condition_p[ch], device_ADC[ch]) for ch in xrange(self.chs)]
 
             condition_cm = np.bitwise_and((np.abs(device_signal_cmc) < np.multiply(self.cm_cut, self.device_ADC_sigma_cmc[:, :self.slide_leng], dtype='float128')), not_masked_array)
-            self.device_cm[:self.slide_leng] = np.array([np.extract(condition_cm[:, ev], device_signal_cmc[:, ev]).mean(dtype='float128') for ev in xrange(self.slide_leng)])
+            self.device_cm[:self.slide_leng] = np.array([np.extract(condition_cm[:, ev], device_signal_cmc[:, ev]).mean(dtype='float128') if condition_cm[:, ev].any() else 0 for ev in xrange(self.slide_leng)])
             cm_array = np.array([self.device_cm[:self.slide_leng] for ch in xrange(self.chs)], 'float32')
             device_ADC_cmc = np.subtract(device_ADC, cm_array, dtype='float128')
             device_signal_cmc = np.subtract(device_ADC_cmc, self.device_ADC_mean_cmc[:, :self.slide_leng], dtype='float128')

TestAreas.py
@@ -251,11 +251,11 @@ class TestAreas:
 		temp0fact = (buffers[-1] - buffers[0]) / (2 * temp0.sum(dtype='float64') - temp0[0] - temp0[-1])
 		xarrayerrs = np.multiply(temp0, temp0fact, dtype='float64')
 
-		yarray = np.array([self.trans_grid.histo['signal_noise_buffer_{b}_{s}_adc'.format(s=suffix, b=buff)].GetRMS() for buff in buffers], dtype='float64')
-		yarrayerrs = np.array([self.trans_grid.histo['signal_noise_buffer_{b}_{s}_adc'.format(s=suffix, b=buff)].GetRMSError() for buff in buffers], dtype='float64')
+		ymeanarray = np.array([self.trans_grid.histo['signal_noise_buffer_{b}_{s}_adc'.format(s=suffix, b=buff)].GetRMS() for buff in buffers], dtype='float64')
+		ymeanarrayerrs = np.array([self.trans_grid.histo['signal_noise_buffer_{b}_{s}_adc'.format(s=suffix, b=buff)].GetRMSError() for buff in buffers], dtype='float64')
 		# maxy = yarray.max() + yarrayerrs.max()
 
-		tgraph = ro.TGraphErrors(int(buffers.size), buffers.astype('float64'), yarray, xarrayerrs, yarrayerrs)
+		tgraph = ro.TGraphErrors(int(buffers.size), buffers.astype('float64'), ymeanarray, xarrayerrs, ymeanarrayerrs)
 		graphname = 'signal_noise_Vs_buffer_sizes_in_adc_{s}'.format(s=suffix)
 		tgraph.SetNameTitle('g_' + graphname, 'g_' + graphname)
 		tgraph.GetXaxis().SetTitle('buffer size')
@@ -265,6 +265,24 @@ class TestAreas:
 		tgraph.SetMarkerColor(ro.kBlack)
 		tgraph.SetLineColor(ro.kBlack)
 		self.trans_grid.graph[graphname] = tgraph
+		self.trans_grid.canvas[graphname] = ro.TCanvas('c_' + graphname, 'c_' + graphname, 1)
+		self.trans_grid.graph[graphname].Draw('ALP')
+		SetDefault1DCanvasSettings(self.trans_grid.canvas[graphname])
+		self.PositionCanvas(graphname)
+		
+		ymeanncarray = np.array([self.trans_grid.histo['signal_noise_NC_chs_buffer_{b}_{s}_adc'.format(b=buff, s=suffix)].GetRMS() for buff in buffers], dtype='float64')
+		ymeanncarrayerrs = np.array([self.trans_grid.histo['signal_noise_NC_chs_buffer_{b}_{s}_adc'.format(b=buff, s=suffix)].GetRMSError() for buff in buffers], dtype='float64')
+
+		tgraphnc = ro.TGraphErrors(int(buffers.size), buffers.astype('float64'), ymeanncarray, xarrayerrs, ymeanncarrayerrs)
+		graphname = 'signal_noise_NC_chs_Vs_buffer_sizes_in_adc_{s}'.format(s=suffix)
+		tgraphnc.SetNameTitle('g_' + graphname, 'g_' + graphname)
+		tgraphnc.GetXaxis().SetTitle('buffer size')
+		tgraphnc.GetYaxis().SetTitle('signal noise [ADC]')
+		# tgraphnc.GetYaxis().SetRangeUser(0, maxy)
+		tgraphnc.SetMarkerStyle(8)
+		tgraphnc.SetMarkerColor(ro.kBlack)
+		tgraphnc.SetLineColor(ro.kBlack)
+		self.trans_grid.graph[graphname] = tgraphnc
 		self.trans_grid.canvas[graphname] = ro.TCanvas('c_' + graphname, 'c_' + graphname, 1)
 		self.trans_grid.graph[graphname].Draw('ALP')
 		SetDefault1DCanvasSettings(self.trans_grid.canvas[graphname])