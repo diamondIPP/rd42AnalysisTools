90327ad1a26650e52a9444190f5e39561013be11
diegoasanz <diegoasanz@gmail.com>
2019-09-29 04:57:19 +0200

    added methods to create tree with currents data and run data

--- Currents.py
+++ Currents.py
@@ -24,6 +24,7 @@ from Utils import *
 # CONSTANTS
 # ====================================
 evts_per_file = 10000
+tree_name = 'hv_currents'
 # ====================================
 # CLASS FOR THE DATA
 # ====================================
@@ -51,6 +52,11 @@ class Currents:
         self.runs_info = None
         self.runs = []
         self.LoadRunsInfo()
+
+        self.hv_channels = []
+        self.hv_channels_root = ro.TFile()
+        self.hv_channels_tree = ro.TTree()
+        self.CheckHVChannelsROOTFiles()
 
         self.Histos = {}
         self.Stuff = []
@@ -109,8 +115,9 @@ class Currents:
                                 volt_i = 1000 * int(volt_i.strip('K').strip('k')) if 'K' in volt_i or 'k' in volt_i else int(volt_i)
                             numev_i = line.split()[8] if IsInt(line.split()[0]) else line.split()[9]
                             numev_i = 1000 * int(numev_i.strip('K').strip('k')) if 'K' in numev_i or 'k' in numev_i else int(numev_i)
-                            temp_runs_info[run_i] = {'dia': diam_i, 'bias': volt_i, 'num_ev': numev_i, 't_ini': -1.0, 't_end': -1.0}
-                            print run_i, diam_i, volt_i, numev_i
+                            hv_folder_i = Get_From_User_Line('HV folder as it is in the currents logs directory for run ' + run_i + ' with voltage ' + str(volt_i) + ' and DUT ' + diam_i, update=False)
+                            temp_runs_info[run_i] = {'dia': diam_i, 'bias': volt_i, 'num_ev': numev_i, 't_ini': -1.0, 't_end': -1.0, 'hv_ch': hv_folder_i}
+                            print run_i, diam_i, volt_i, numev_i, hv_folder_i
                 self.runs_info = temp_runs_info
                 self.GetDictOfRawFiles()
                 self.DumpRunsInfoInJson(force_recreate)
@@ -160,7 +167,7 @@ class Currents:
                     except Exception:
                         print run_i
                         ipdb.set_trace()
-                    self.runs_evets_times[run_i] = {}
+                    self.runs_evets_times[run_i] = OrderedDict()
                     # for pos_i, [ti_i, tf_i] in enumerate(zip(raws_run_i_ti, raws_run_i_tf)):
                     #     if (pos_i + 1) * evts_per_file < self.runs_info[run_i]['num_ev']:
                     #         for ev in xrange(pos_i * evts_per_file, (pos_i + 1) * evts_per_file):
@@ -186,7 +193,119 @@ class Currents:
             print 'Created pickle file with the event times for all the runs in the testbeam'
 
 
-    
+    def CheckHVChannelsROOTFiles(self, load_option='READ'):
+        self.hv_channels = sorted([dir_i for dir_i in os.listdir(self.currents_logs_dir) if dir_i in list(set([elem_i['hv_ch'] for elem_i in self.runs_info.values()]))])
+        for hv_ch in self.hv_channels:
+            if load_option.lower() in ['recreate', 'new']:
+                self.hv_channels_tree = None
+                self.hv_channels_root = ro.TFile(self.currents_logs_dir + '/' + hv_ch + '.root', load_option)
+                self.hv_channels_tree = ro.TTree(tree_name, tree_name)
+                self.FillCurrentsTreeOfHVChannelFolder(hv_ch)
+
+    def FillCurrentsTreeOfHVChannelFolder(self, hvfolder):
+        print 'Filling {t} tree for HV folder {f}'.format(t=self.hv_channels_tree, f=hvfolder)
+        if os.path.isdir(self.currents_logs_dir + '/' + hvfolder):
+            files = glob(self.currents_logs_dir + '/' + hvfolder + '/*.log')
+            files.sort(key=lambda x: os.path.getmtime(x))
+            # create branches
+            hv_ch = ro.TString(hvfolder)
+            # hv_ch_time = ro.TDatime()
+            hv_ch_time = ro.TTimeStamp()
+            hv_ch_volt = np.zeros(1, 'f4')
+            hv_ch_curr = np.zeros(1, 'f4')
+            hv_ch_run = np.zeros(1, 'uint16')
+            # hv_ch_ev_i = np.zeros(1, 'int32')
+            # hv_ch_ev_f = np.zeros(1, 'int32')
+            hv_ch_dia = ro.TString()
+            # assign branches to tree
+            self.hv_channels_tree.Branch('channelHV', hv_ch)
+            self.hv_channels_tree.Branch('voltageHV', hv_ch_volt, 'voltageHV/F')
+            self.hv_channels_tree.Branch('currentHV', hv_ch_curr, 'currentHV/F')
+            self.hv_channels_tree.Branch('timeHV', hv_ch_time)
+            self.hv_channels_tree.Branch('runHV', hv_ch_run, 'runHV/s')
+            # self.hv_channels_tree.Branch('eventIni', hv_ch_ev_i, 'eventIni/I')
+            # self.hv_channels_tree.Branch('eventFin', hv_ch_ev_f, 'eventFin/I')
+            self.hv_channels_tree.Branch('diaHV', hv_ch_dia)
+
+            print 'Reading all files inside', hvfolder
+            lines_f = []
+            bar = CreateProgressBarUtils(len(files))
+            bar.start()
+            for it, file_i in enumerate(files):
+                tempf = file_i.strip('.log').split('_')
+                year_f, month_f, day_f = tempf[-6], tempf[-5], tempf[-4]
+                with open(file_i, 'r') as logf:
+                    for line in logf:
+                        lines_f.append('{y} {m} {d} {l}'.format(y=year_f, m=month_f, d=day_f, l=line))
+                    bar.update(it + 1)
+            bar.finish()
+            print 'Removing lines not used...', ; sys.stdout.flush()
+            list_f = [[int(line.split()[0]), int(line.split()[1]), int(line.split()[2]), line.split()[3], float(line.split()[4]), float(line.split()[5])] for line in lines_f if len(line.strip('\n').split()) > 5 and IsFloat(line.split()[4]) and IsFloat(line.split()[5])]
+            print 'Done'
+            print 'Filling tree:'
+            bar = CreateProgressBarUtils(len(list_f))
+            bar.start()
+            for it, line in enumerate(list_f):
+                hv_ch_dia.Clear()
+                year_f, month_f, day_f = int(line[0]), int(line[1]), int(line[2])
+                HMS = line[3].split(':')
+                hour_f, min_f, sec_f = int(HMS[0]), int(HMS[1]), int(HMS[2])
+                volt_f, curr_f = line[4], line[5]
+                # hv_ch_time.Set(year_f, month_f, day_f, hour_f, min_f, sec_f)
+                hv_ch_time.Set(year_f, month_f, day_f, hour_f, min_f, sec_f, 0, False, 0)
+                run_f = self.GuessRunFromRunInfo(hvfolder, hv_ch_time.AsDouble())
+                dia_f = self.runs_info[run_f]['dia'] if run_f in self.runs_info.keys() else 'None'
+                # evi_f, evf_f = self.FindEventIniAndFinFromTimeStamp(hv_ch_time.Convert(), run_f)
+                # evi_f, evf_f = -1, -1
+                # evi_f, evf_f = self.FindEventIniAndFinFromTimeStamp(hv_ch_time.AsDouble(), run_f)
+                hv_ch_volt.fill(volt_f)
+                hv_ch_curr.fill(curr_f)
+                hv_ch_run.fill(int(run_f))
+                # hv_ch_ev_i.fill(evi_f)
+                # hv_ch_ev_f.fill(evf_f)
+                hv_ch_dia.Append(dia_f)
+                self.hv_channels_tree.Fill()
+                bar.update(it + 1)
+
+            #
+            # for it, file_i in enumerate(files):
+            #     with open(file_i, 'r') as logf:
+            #         lines_f = logf.readlines()
+            #         # remove lines not used
+            #         list_f = [[line.strip('\n').split()[0], float(line.strip('\n').split()[1]), float(line.strip('\n').split()[2])] for line in lines_f if len(line.strip('\n').split()) > 2 and IsFloat(line.strip('\n').split()[1]) and IsFloat(line.strip('\n').split()[2])]
+            #         hv_ch_dia.Clear()
+            #         for line in list_f:
+            #             HMS = line[0].split(':')
+            #             hour_f, min_f, sec_f = int(HMS[0]), int(HMS[1]), int(HMS[2])
+            #             volt_f, curr_f = line[1], line[2]
+            #             hv_ch_time.Set(year_f, month_f, day_f, hour_f, min_f, sec_f, 0, False, 0)
+            #             run_f = self.GuessRunFromRunInfo(hvfolder, hv_ch_time)
+            #             dia_f = self.runs_info[str(run_f)]['dia'] if str(run_f) in self.runs_info.keys() else 'None'
+            #             evi_f, evf_f = self.FindEventIniAndFinFromTimeStamp(hv_ch_time, run_f)
+            #             hv_ch_volt.fill(volt_f)
+            #             hv_ch_curr.fill(curr_f)
+            #             hv_ch_run.fill(run_f)
+            #             hv_ch_ev_i.fill(evi_f)
+            #             hv_ch_ev_f.fill(evf_f)
+            #             hv_ch_dia.Append(dia_f)
+            #             self.hv_channels_tree.Fill()
+            #             bar.update(it + 1)
+            self.hv_channels_root.Write()
+            self.hv_channels_root.Close()
+            bar.finish()
+
+    def GuessRunFromRunInfo(self, hvfolder, timestamp):
+        for run_i, items in self.runs_info.iteritems():
+            if hvfolder in items['hv_ch']:
+                if items['t_ini'] <= timestamp <= items['t_end']:
+                    return run_i
+        return '0'
+
+    def FindEventIniAndFinFromTimeStamp(self, timestamp, run_f='0'):
+        if run_f in self.runs_info.keys():
+            cond = np.less_equal(np.abs(np.subtract(timestamp, self.runs_evets_times[run_f].values())), 0.5)
+            return cond.argmax(), self.runs_info[run_f]['num_ev'] - 1 - cond[::-1].argmax()
+        return -1, -1
 
 if __name__ == "__main__":
     parser = OptionParser()
